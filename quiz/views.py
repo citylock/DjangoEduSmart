477c4a566fd3b60e7962c6d58af09c62bd6a7b21
citylock <citylock77@gmail.com>
2019-12-28 07:21:04 +1100

    Django the first web pages

---
+++ quiz/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request): 
+    text_var = "This is my first Django app web response!!"
+    return HttpResponse(text_var)
+    
