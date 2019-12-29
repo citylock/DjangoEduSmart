d04b695cf1b48f53d2a6c9c86fc0d04ea8a5c27f
citylock <citylock77@gmail.com>
2019-12-29 13:58:11 +1100

    2019.12.19 before the removal

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Category, Quiz
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'description']
+    prepopulated_fields = { 'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['slug', 'category','qtype', 'question', 'question_image', 'difficulty', 'hint','created', 'updated']
+    list_editable = ['category', 'qtype', 'question', 'question_image', 'difficulty', 'hint']
+    prepopulated_fields = {'slug': ('number', 'question',)}
+    list_per_page = 20
+
+admin.site.register(Quiz, QuizAdmin)
