923c975b78d212a4c2ed7ddb14b237bd72127b84
citylock <citylock77@gmail.com>
2020-01-28 03:23:56 +1100

    Test 2

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,19 +1,10 @@
 from django.contrib import admin
 
-from .models import Category, Quiz
+from .models import Category
 # Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'description']
-    prepopulated_fields = { 'slug': ('name',)}
+    list_display = ['category']
 
 admin.site.register(Category, CategoryAdmin)
 
-
-class QuizAdmin(admin.ModelAdmin):
-    list_display = ['slug', 'category','qtype', 'question', 'question_image', 'difficulty', 'hint','created', 'updated']
-    list_editable = ['category', 'qtype', 'question', 'question_image', 'difficulty', 'hint']
-    prepopulated_fields = {'slug': ('number', 'question',)}
-    list_per_page = 20
-
-admin.site.register(Quiz, QuizAdmin)
--- quiz/models.py
+++ quiz/models.py
@@ -2,36 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
     # Type of exam : opportunity class, selective, naplan, ... 
-    name = models.CharField(max_length=250, unique=True)
-    slug = models.SlugField(max_length=250, unique=True)
+    category = models.CharField(max_length=250, unique=True)
     description = models.TextField(blank=True)
 
     class Meta: 
-        ordering = ('name', )
+        ordering = ('category', )
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
     def __str__(self):
-        return '{}'.format(self.name)
+        return '{}'.format(self.category)
 
-
-class Quiz(models.Model):
-    number = models.DecimalField(max_digits=10, decimal_places=0, unique=True)
-    slug = models.SlugField(max_length=250, unique=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    # qtype : text, image, .. 
-    qtype = models.CharField(max_length=50)
-    question = models.TextField()
-    question_image = models.ImageField(upload_to="question", blank=True)
-    difficulty = models.DecimalField(max_digits=10, decimal_places=1, default = 1000)
-    hint = models.TextField(blank=True)
-    created =  models.DateField(auto_now_add=True)
-    updated = models.DateField(auto_now=True)
-
-    class Meta: 
-        ordering = ('id', )
-        verbose_name = 'quiz'
-        verbose_name_plural = 'quizes'
 
