237c8cdbdc5eb0a5d0981d3e38a109c38430de96
citylock <citylock77@gmail.com>
2020-01-30 03:45:49 +1100

    models for category, quiz, question and answers

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Quiz
+from .models import Category, Quiz, Question, Answer
 # Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -12,3 +12,13 @@ class QuizAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 admin.site.register(Quiz, QuizAdmin)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['content']
+
+admin.site.register(Question, QuestionAdmin)
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['question', 'content', 'correct']
+
+admin.site.register(Answer, AnswerAdmin)
--- quiz/models.py
+++ quiz/models.py
@@ -20,7 +20,7 @@ class Category(models.Model):
 class Quiz(models.Model): 
     title = models.CharField(max_length=200, unique=True)
     description = models.TextField(blank=True)
-    slug = models.SlugField(max_length=200, blank=False, help_text="a user friendly url")
+    slug = models.SlugField(max_length=200, blank=False, help_text="a user friendly url, should be unique")
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.CASCADE)
     random_order = models.BooleanField(blank=False, default=False, help_text="Display the questions in a random order or as they set..")
     max_questions = models.PositiveIntegerField(blank=True, null=True, help_text="Number of questions to be answered on each attempt.")
@@ -36,4 +36,37 @@ class Quiz(models.Model):
         return '{}'.format(self.title)
 
 
-    
+
+class Question(models.Model):
+    """
+    Base class for all question types. 
+    """
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, blank=True, null=True, on_delete=models.CASCADE)
+    figure = models.ImageField(upload_to='uploads/%Y/%m/%d', blank=True, null=True)
+    content = models.CharField(max_length=250, blank=False, help_text="Enter the question text that you want displayed")
+    explanation = models.TextField(max_length=1000, blank=True, help_text="Explanation to be shown after the question has been answered.")
+
+    class Meta: 
+        verbose_name = 'question'
+        verbose_name_plural = 'questions'
+        ordering = ['category']
+
+    def __str__(self):
+        return "{}-{}".format(self.pk, self.content)
+
+class Answer(models.Model):
+    """
+    list of answers. 
+    """
+    question = models.ForeignKey(Question, blank=False, null=False, on_delete=models.CASCADE)
+    content = models.CharField(max_length=100, blank=True, null=True, help_text="Enter the answer text that you want displayed")
+    figure = models.ImageField(upload_to='uploads/%Y/%m/%d', blank=True, null=True)
+    correct = models.BooleanField(blank=False, default=False, help_text="Is this a correct answer?")
+
+    def __str__(self):
+        return self.content
+
+    class Meta: 
+        verbose_name = "answer"
+        verbose_name_plural = "answers"
